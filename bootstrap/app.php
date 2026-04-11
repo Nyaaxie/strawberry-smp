f37ff455c39fe99ff40b8eee75d05d28a05431c4
James Brizuela <jamesbrizuela513@gmail.com>
2026-04-11 23:48:29 +0800

    force https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,18 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\URL;
+
+URL::forceScheme('https');
+
 
 return Application::configure(basePath: dirname(__DIR__))
+
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
@@ -16,3 +22,5 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+   
+    
